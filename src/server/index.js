6ea19bca0770d9eb645a1dcb0f6c1e8e815b1af3
Sharmaz <irae45@gmail.com>
2016-05-06 15:37:24 -0500

    Servimos archivos estaticos

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,6 @@ import express from 'express'
 const app = express()
 const port = process.env.PORT || 3000
 
-app.listen(port, function () {
-  console.log(`Server running on ${port}`)
-})
+app.use(express.static('public'))
+
+app.listen(port, () => console.log(`Server running on ${port}`))
